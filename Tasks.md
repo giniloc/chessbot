142bf68945df7ced22a89b648b5a7272392ad0cb
Your Name <simon.jonckheere@gmail.com>
2024-08-01 22:42:33 +0200

    Shimoan begint zijn werk

---
+++ Tasks.md
@@ -0,0 +1,29 @@
+### TAAKJES :))
+
+### GIL 
+
+1. Piece code herschrijven 
+2. Coordinate class 
+3. Board class 
+4. Rook implementen 
+5. Bishop implementen
+
+### SIMON 
+
+1. Pawn 
+2. King 
+
+### Open problems
+
+1. Queen valid moves checken
+2. Knight valid moves checken
+
+### Stappenplan 
+
+1. Valid moves maken 
+2. Valid moves testen 
+3. Valid moves laten oplichten in UI 
+4. Pieces echt laten bewegen (Zonder UI)
+5. Bewegen testen (Zonder UI)
+6. UI laten volgen 
+7. Bot implementen
--- src/main/java/Pieces/Piece.java
+++ src/main/java/Pieces/Piece.java
@@ -1,5 +1,6 @@
 package Pieces;
 
+import java.util.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -8,17 +9,26 @@ public abstract class Piece extends ImageView {
     protected int row;
     protected int col;
     protected boolean isWhite;
+    //TODO: van board klasse maken in plaats van 2d array
+    protected Piece[][] board;
 
-    public Piece(int row, int col, boolean isWhite, String imageName) {
+    public Piece(int row, int col, boolean isWhite, String imageName, Piece[][] board) {
         this.row = row;
         this.col = col;
         this.isWhite = isWhite;
+        this.board = board;
         // Laad de afbeelding en stel deze in op de ImageView
         Image image = new Image(getClass().getResourceAsStream("/" + imageName));
         this.setImage(image);
     }
 
-    public abstract boolean isValidMove(int newRow, int newCol, Piece[][] board);
+
+    //TODO: board als internal value zetten
+    public abstract boolean isValidMove(int newRow, int newCol);
+
+    //Return: alle legal coords waar piece naar kan moven in vorm van int list met 2 values (row, column)
+    //TODO: int[] vervangen door coords klasse
+    public abstract ArrayList<int[]> getValidMoves();
 
     public void move(int newRow, int newCol) {
         this.row = newRow;
