e6493667cd44ea4f84236dce9f44a605275fb15a
Your Name <simon.jonckheere@gmail.com>
2024-08-01 23:08:18 +0200

    merge conflictje opgelost

--- src/main/java/Pieces/Pawn.java
+++ src/main/java/Pieces/Pawn.java
@@ -1,15 +1,14 @@
 package Pieces;
 
+import Utils.Board;
 import Utils.Coordinate;
 
 import java.util.ArrayList;
-import Utils.Board;
 
 public class Pawn extends Piece {
     public Pawn(int row, int col, boolean isWhite, Board board) {
         super(row, col, isWhite, isWhite ? "whitePawn.png" : "blackPawn.png", board);
     }
-
 
     @Override
     public boolean isValidMove(int newRow, int newCol) {
@@ -18,6 +17,28 @@ public class Pawn extends Piece {
 
     @Override
     public ArrayList<Coordinate> getValidMoves() {
-        return null;
+        ArrayList<Coordinate> validMoves = new ArrayList<>();
+
+        if (isWhite){
+
+            /*
+
+            Huidige positie checken
+            check of pawn al bewogen heeft
+            TODO: boolean hasMoved toevoegen
+            Opvragen van potentiele moves
+                1.  linksvoor, 1 vooruit, rechtsvoor
+                2. Als nog niet bewogen, 2 vooruit
+                3. Checken voor en passant (5de rank voor wit, 4de rank voor zwart)
+                    TODO: boolean justJumped toevoegen en 1 beurt true laten staan.
+                    TODO: uitzoeken hoe het best de en passant weg te halen
+             Check voor elke potentiele move of legal is en voeg toe aan validmoves coordlist
+
+             */
+        } else {
+
+        }
+
+        return validMoves;
     }
 }
